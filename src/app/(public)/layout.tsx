7b22e66754eb71c672abb6939d8df83d47634def
Chidera Akaeme <chideraakaeme@gmail.com>
2026-03-26 12:50:24 +0100

    worked on the login,register and testimonial page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -5,7 +5,7 @@ export default function PublicLayout({
 }) {
   return (
     <div className="flex min-h-screen  bg-linear-to-br from-orange-50 via-yellow-50 to-white px-4 py-8">
-      <div className="w-full ">{children}</div>
+      <div className="w-full">{children}</div>
     </div>
   );
 }
